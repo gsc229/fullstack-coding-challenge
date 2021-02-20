ff022c2a65e231a19c90778be65b1c1dcdedf70e
greg <gsc229@gmail.com>
2021-02-19 21:11:29 -0500

    first route complete

--- testApp/complaint_app/views.py
+++ testApp/complaint_app/views.py
@@ -6,16 +6,20 @@ from rest_framework import status
 # Create your views here.
 
 class ComplaintViewSet(viewsets.ModelViewSet):
-  http_method_names = ['get']
-  serializer_class = ComplaintSerializer
+
   def list(self, request):
     # Get all complaints from the user's district
-    return Response()
+    userprofile = UserProfile.objects.filter(user=request.user).first()
+    user_dist = f"NYCC0{getattr(userprofile, 'district')}" if len(getattr(userprofile, 'district')) == 1 else f"NYCC{getattr(userprofile, 'district')}"
+    queryset = Complaint.objects.filter(account=user_dist)
+    serializer = ComplaintSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 class OpenCasesViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
   def list(self, request):
     # Get only the open complaints from the user's district
+    
     return Response()
 
 class ClosedCasesViewSet(viewsets.ModelViewSet):

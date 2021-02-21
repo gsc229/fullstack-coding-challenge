a3cd8758ae2bb789611aa34d8a5a70a8299620b5
greg <gsc229@gmail.com>
2021-02-21 00:41:59 -0500

    refacotred view sets to use getDistString method. TopComplaints view set now returns top three array

--- testApp/complaint_app/views.py
+++ testApp/complaint_app/views.py
@@ -6,25 +6,21 @@ from rest_framework import status
 from django.db.models import Count
 # Create your views here.
 
-def getDistrictString(userprofile):
+def getDistrictString(user):
+  userprofile = UserProfile.objects.filter(user=user).first()
   dist_num = getattr(userprofile, 'district')
   return f"NYCC0{dist_num}" if len(dist_num) == 1 else f"NYCC{dist_num}"
+
 
 class ComplaintViewSet(viewsets.ModelViewSet):
   http_method_names = ['get']
   serializer_class = ComplaintSerializer
   queryset = Complaint.objects.all()
-  def get_queryset(self):
-    print(self.kwargs)
-    print(self.get_extra_action_url_map)
-    userprofile = UserProfile.objects.filter(user=self.request.user).first()
-    user_dist = getDistrictString(userprofile)
-    queryset = Complaint.objects.filter(account=user_dist)
-    return queryset
 
   def list(self, request):
     # Get all complaints from the user's district
-    queryset = self.get_queryset()
+    user_dist = getDistrictString(request.user)
+    queryset = self.queryset.filter(account=user_dist)
     serializer = ComplaintSerializer(queryset, many=True)
     return Response(serializer.data)
 
@@ -35,8 +31,7 @@ class OpenCasesViewSet(viewsets.ModelViewSet):
 
   def list(self, request):
     #Get only the open complaints from the user's district
-    userprofile = UserProfile.objects.filter(user=request.user).first()
-    user_dist = getDistrictString(userprofile)
+    user_dist = getDistrictString(user=request.user)
     queryset = self.queryset.filter(account=user_dist, closedate__isnull=True)
     serializer = ComplaintSerializer(queryset, many=True)
     return Response(serializer.data)
@@ -48,8 +43,7 @@ class ClosedCasesViewSet(viewsets.ModelViewSet):
 
   def list(self, request):
     # Get only complaints that are close from the user's district
-    userprofile = UserProfile.objects.filter(user=request.user).first()
-    user_dist = getDistrictString(userprofile)
+    user_dist = getDistrictString(user=request.user)
     queryset = self.queryset.filter(account=user_dist, closedate__isnull=False)
     serializer = ComplaintSerializer(queryset, many=True)
     return Response(serializer.data)
@@ -61,8 +55,7 @@ class TopComplaintTypeViewSet(viewsets.ModelViewSet):
 
   def list(self, request):
     # Get the top 3 complaint types from the user's district
-    userprofile = UserProfile.objects.filter(user=request.user).first()
-    user_dist = getDistrictString(userprofile)
+    user_dist = getDistrictString(user=request.user)
     annotatedSet = self.queryset.filter(complaint_type__isnull=False).values('complaint_type').order_by('complaint_type').annotate(count=Count('complaint_type')).order_by('-count')
-        
-    return Response(annotatedSet)
+    top_three =  [ complaint['complaint_type'] for complaint in annotatedSet][:3]
+    return Response({"top_three": top_three, "data": annotatedSet})

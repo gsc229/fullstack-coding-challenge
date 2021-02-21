a3cb5377129f825fa1db4b49be499e4c991e1fa9
greg <gsc229@gmail.com>
2021-02-21 03:16:33 -0500

    login form styles complete

--- testApp/frontend/src/Auth/Login.js
+++ testApp/frontend/src/Auth/Login.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axiosWithAuth from '../utils/axiosWIthAuth'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const Login = () => {
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+
+  })
+
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = () => {
+
+  }
+
   return (
     <div>
-      <h1>Login </h1>
+      <h4>Login</h4>
+      <div className='login-form-container'>
+        <Form className='login-form'>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+            onChange={handleChange} 
+            name='username'
+            value={credentials.username}
+            type="text" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your info with anyone else.
+            </Form.Text>
+          </Form.Group>
+  
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+            onChange={handleChange} 
+            name='password'
+            value={credentials.password}
+            type="password" placeholder="Password" />
+          </Form.Group>
+          <Button 
+          onSubmit={handleSubmit}
+          variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   )
 }

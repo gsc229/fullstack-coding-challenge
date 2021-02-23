f5d68f14209fe546132aba721c79adbf0e334238
greg <gsc229@gmail.com>
2021-02-23 02:18:01 -0500

    had to remove the conditional rendering in the rows map bc it was interfering with the global filter. Will need to troubleshoot more on how to replace null values with 'Closed' or 'not provided'

--- testApp/frontend/src/Components/Dashboard/DataTable.js
+++ testApp/frontend/src/Components/Dashboard/DataTable.js
@@ -1,8 +1,8 @@
 import React, { useMemo } from 'react'
 import Table from 'react-bootstrap/Table'
-import { useTable } from 'react-table'
+import { useTable, useGlobalFilter } from 'react-table'
 import { COLUMNS } from './helpers/columns'
-import { complaintFieldTitle } from './helpers/complatintFieldTitle'
+import GlobalFilter from './GlobalFilter'
 
 const DataTable = ({complaintsData}) => {
 
@@ -16,64 +16,67 @@ const DataTable = ({complaintsData}) => {
     headerGroups,
     footerGroups,
     rows, 
-    prepareRow
-  } = useTable({columns, data: [...data]})
+    prepareRow,
+    state,
+    setGlobalFilter
+  } = useTable(
+    {columns, data}, 
+    useGlobalFilter)
+
+  const { globalFilter } = state
 
   return (
-    <Table responsive striped bordered hover size="sm" {...getTableProps}>
-      <thead>
-          
-          {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+    <>
+      
+      <GlobalFilter filter={ globalFilter } setFilter={ setGlobalFilter } />
+      <Table responsive striped bordered hover size="sm" {...getTableProps}>
+        <thead>
+            
+            {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
             ))}
-          </tr>
-          ))}
-        
-      </thead>
-      <tbody>
-      {rows.map((row, i) => {
-          /* console.log({row}) */
-          row.values.closedate = row.values.closedate ? `Closed ${row.values.closedate}` : 'Open'
-          row.values.council_dist = row.values.council_dist ? row.values.council_dist : 'not provided'
-          row.values.city = row.values.city ? row.values.city : 'not provided'
-          row.values.borough = row.values.borough ? row.values.borough : 'not provided'
-          row.values.community_board = row.values.community_board === ' ' ? 'not provided' : row.values.community_board 
-          row.values.zip = row.values.zip ? row.values.zip : 'not provided'
           
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (
-                  <td {...cell.getCellProps()}>
-                    {cell.render('Cell')}
-                  </td>
-                )
-              })}
-            </tr>
-          )
-        })}
-      </tbody>
-      <tfoot>
-        {
-          footerGroups.map(footerGroup => (
-            <tr {...footerGroup.getFooterProps}>
-              {
-                footerGroup.headers.map(column =>(
-                  <td{...column.getFooterProps}>
-                  {
-                    column.render('Footer')
-                  }
-                  </td>
-                ))
-              }
-            </tr>
-          ))
-        }
-      </tfoot>
-    </Table>
+        </thead>
+        <tbody>
+        {rows.map((row, i) => {
+            
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot>
+          {
+            footerGroups.map(footerGroup => (
+              <tr {...footerGroup.getFooterProps}>
+                {
+                  footerGroup.headers.map(column =>(
+                    <td{...column.getFooterProps}>
+                    {
+                      column.render('Footer')
+                    }
+                    </td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </tfoot>
+      </Table>
+    </>
   )
 }
 
